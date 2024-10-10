ab0b9b0de97fa4109ed6e97e55085df7cc20b027
Soleh Ahmad Rafli <rafli0882@gmail.com>
2024-10-10 11:17:18 +0700

    Initial commit

---
+++ application/views/dosen/tambah.php
@@ -0,0 +1,22 @@
+<div class="container">
+  <div class="row-mt-6">
+    <div class="col md-6">
+      <div class="card">
+        <div class="card-header">
+          Form tambah Data Dosen
+        </div>
+        <div class="card-body">
+          <form action="" method="post">
+            <div class="form-group">
+              <label for="nip">NIP</label>
+              <input type="text" class="form-control" id="nip" name="nip">
+            </div>
+            <div class="form-group">
+              <label for="namadosen">NAMA DOSEN</label>
+              <input type="text" class="form-control" id="namadosen" name="namadosen">
+            </div>
+            <button type="submit" name="tambah" class="btn btn-primary flaot-end">tambah</button>
+          </form>
+        </div>
+      </div>
+    </div>
